bbd63930b7e697be8edc63bfb874b90b804fa8c3
Nicholas Nagy <ygannicholas@gmail.com>
2021-07-15 12:28:50 -0400

    fix: remove mesh scaling

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -67,34 +67,32 @@ void Renderer::drawMesh(VertexArray& va, Shader& shader, glm::mat4 view, glm::ma
 {
 	va.bind();
 	shader.bind();
-	glm::mat4 model = glm::mat4(1.0f);
 
 	shader.setUniform4Mat("view", view);
 	shader.setUniform4Mat("projection", projection);
-	shader.setUniform4Mat("model", model);
 	shader.setUniform4Vec("ourColor", glm::vec4(0.5, 0.5, 0.5, 0.0f));
+
+	glm::mat4 model;
+	glm::mat4 model2;
 
 	for (float current = -50.0f; current <= 50.0f; current += 1.0f) {
 
 		// center line * scaling
-		model = glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f * scaleFactor, 0.0f, current * scaleFactor)) 
-			* glm::scale(glm::mat4(1.0f), glm::vec3(100 * scaleFactor));
+		model = glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f, 0.0f, current)) 
+			* glm::scale(glm::mat4(1.0f), glm::vec3(100));
 		shader.setUniform4Mat("model", model);
 		GLCall(glDrawArrays(GL_LINES, 0, 2));
 
 		// rotate line * center line * scaling
-		glm::mat4 model2 = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)) 
-			* glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f * scaleFactor, 0.0f, current * scaleFactor)) 
-			* glm::scale(glm::mat4(1.0f), glm::vec3(100 * scaleFactor));
+		model2 = glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f)) 
+			* glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f, 0.0f, current)) 
+			* glm::scale(glm::mat4(1.0f), glm::vec3(100));
 		shader.setUniform4Mat("model", model2);
 		GLCall(glDrawArrays(GL_LINES, 0, 2));
 	}
 
-	
-
 	va.unbind();
 	shader.unbind();
-
 }
 
 void Renderer::drawObject(VertexArray& va, Shader& shader, vector<glm::mat4> modelRotMat, vector<glm::mat4> modelTransMat, float scaleFactor) {
